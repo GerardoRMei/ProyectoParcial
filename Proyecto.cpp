673ae8aab4a74e69e76908f1cdf7dba42dc56874
GerardoRMei <gerardozepeda12@gmail.com>
2022-10-27 13:11:19 -0500

    a

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -2,32 +2,35 @@
 #include "ZonaEspecial.h"
 #include "entidad.h"
 #include "users.h"
-#include<thread>
-#include<chrono>
 #include<vector>
 #include<time.h>
+#include<chrono>
+#include<thread>
 #include<pthread.h>
 #include<windows.h>
 #include<stdio.h>
 
 using namespace std;
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 int board_X=25;
 int board_Y=25;
-bool GameOver=false, didWon=false;
+bool GameOver=false, didWon=false, moved=false;
 int movement_counter =50;
 int total_Score=0;
 ZonaEspecial okane[3], SafeSpace;
 entidad enemigo[2], player;
+vector<vector<string>> board;
 
-void GenerateObjects( ZonaEspecial monedas[3], ZonaEspecial &SafeSpace, entidad enemigo[2],entidad &player){
+void GenerateObjects(){
     int randX, randY, randZ, randW;
     for(int i=0;i<3;i++){
         randX = (rand() % board_X);
         randY = (1+rand() % board_Y);
-        monedas[i]= ZonaEspecial(randX,randY,"$");
+        okane[i]= ZonaEspecial(randX,randY,"$");
         randZ = (rand() % board_X);
-        randW = (1+rand() % board_Y);
+        randW = (1+rand() % board_Y-(5*i));
         enemigo[i] = entidad(randZ, randW, "X");
     }
     randX = (rand() % board_X);
@@ -35,22 +38,32 @@ void GenerateObjects( ZonaEspecial monedas[3], ZonaEspecial &SafeSpace, entidad 
     SafeSpace=ZonaEspecial(randX, randY,"0");
     player = entidad(board_X-1,board_Y-2,"P"); // para colocar al borde de la zona
 }
+void instructions(){ //funcion estilo menu para que explicar al usuario.
+    cout<<"\n\n";
+    cout<<"Intructions:  \n";
+    cout<<"Goal: reach the safe zone before running out of time, also avoid the enemies\n";
+    cout<<"1. To move one right type 'r', for 2 spaces right type '2r'"<<"\n";
+    cout<<"2. To move one left type 'l', for 2 spaces left type '2l'"<<"\n";
+    cout<<"3. To move one up type 'u', for 2 spaces up type '2u'"<<"\n";
+    cout<<"4. To move one down type 'd', for 2 spaces down type '2d'"<<"\n";
+    cout<<"5. For diagonal movement type 'dru', 'dlu', 'drd', 'dlu'"<<"\n";
+}
 
-void FillGameBoard(vector<vector<string>> board,ZonaEspecial monedas[3], ZonaEspecial SafeSpace,entidad enemigo[2],entidad &player){
+void FillGameBoard(){
     board.resize(board_Y, vector<string>(board_X));
-
+    instructions();
     for(int i=0;i<board_Y;i++){
         for(int j=0;j<board_X;j++){
             if(i==0 || i == board_Y-1){
                 board[i][j]= "-";
-            }else if(i==monedas[0].getposicionY() && j==monedas[0].getposicionX() && monedas[0].isPisoteado()==false){
-                board[i][j]= monedas[0].getSimbolo();
+            }else if(i==okane[0].getposicionY() && j==okane[0].getposicionX() && okane[0].isPisoteado()==false){
+                board[i][j]= okane[0].getSimbolo();
             }     
-            else if(i==monedas[1].getposicionY() && j==monedas[1].getposicionX() && monedas[1].isPisoteado()==false){
-                board[i][j]= monedas[1].getSimbolo();
+            else if(i==okane[1].getposicionY() && j==okane[1].getposicionX() && okane[1].isPisoteado()==false){
+                board[i][j]= okane[1].getSimbolo();
             }
-            else if(i==monedas[2].getposicionY() && j==monedas[2].getposicionX() && monedas[2].isPisoteado()==false){
-                board[i][j]= monedas[2].getSimbolo();
+            else if(i==okane[2].getposicionY() && j==okane[2].getposicionX() && okane[2].isPisoteado()==false){
+                board[i][j]= okane[2].getSimbolo();
             }
             else if(i==SafeSpace.getposicionY() && j==SafeSpace.getposicionX()){
                 board[i][j]= SafeSpace.getSimbolo();
@@ -76,6 +89,8 @@ void FillGameBoard(vector<vector<string>> board,ZonaEspecial monedas[3], ZonaEsp
         }
         cout<<"|\n";
     }
+    cout<<"\n \n"<<movement_counter<<" Turns left"<<endl;
+    cout<<"Type your move"<<endl;
 }
 
 void checkGameOver(){
@@ -93,94 +108,7 @@ void checkGameOver(){
     }
 }
 
-void instructions(){ //funcion estilo menu para que explicar al usuario.
-    cout<<"\n\n";
-    cout<<"Intructions:  \n";
-    cout<<"Goal: reach the safe zone before running out of time, also avoid the enemies\n";
-    cout<<"1. To move one right type 'r', for 2 spaces right type '2r'"<<"\n";
-    cout<<"2. To move one left type 'l', for 2 spaces left type '2l'"<<"\n";
-    cout<<"3. To move one up type 'u', for 2 spaces up type '2u'"<<"\n";
-    cout<<"4. To move one down type 'd', for 2 spaces down type '2d'"<<"\n";
-    cout<<"5. For diagonal movement type 'dru', 'dlu', 'drd', 'dlu'"<<"\n";
-}
-void *PlayerMove(void *arg){
-    string action;
-    cout<<"Type your move"<<endl;
-    getline(cin,action);
-
-    if(action=="r"){
-        if(player.getposicionX()+1>=board_X){}
-        else{
-            player.setposiciones(player.getposicionX()+1,player.getposicionY());
-        }
-    }
-    else if(action=="2r"){
-            if(player.getposicionX()+2>=board_X){
-            }else{
-                player.setposiciones(player.getposicionX()+2,player.getposicionY());
-            }
-    }
-    else if(action=="l"){
-            if(player.getposicionX()-1<0){}
-            else{
-                player.setposiciones(player.getposicionX()-1,player.getposicionY());
-            }
-    }
-    else if(action=="2l"){
-            if(player.getposicionX()-2<0){}
-            else{
-                player.setposiciones(player.getposicionX()-2,player.getposicionY());
-            }
-    }
-    else if(action=="u"){
-        if(player.getposicionY()-1<=0){}
-        else{
-            player.setposiciones(player.getposicionX(),player.getposicionY()-1);
-        }
-    }
-    else if(action=="2u"){
-            if(player.getposicionY()-2<=0){}
-            else{
-            player.setposiciones(player.getposicionX(),player.getposicionY()-2);
-            }
-    }
-    else if(action=="d"){
-        if(player.getposicionY()+1>=board_Y-1){}
-        else{
-        player.setposiciones(player.getposicionX(),player.getposicionY()+1);
-        }
-    }
-    else if(action=="2d"){
-        if(player.getposicionY()+2>=board_Y-1){}
-        else{
-        player.setposiciones(player.getposicionX(),player.getposicionY()+2);
-        }    
-    }
-    else if(action=="dru"){
-        if(player.getposicionX()+1>=board_X || player.getposicionY()-1<=0){}
-        else{
-        player.setposiciones(player.getposicionX()+1,player.getposicionY()-1);
-        }
-    }else if(action=="dlu"){
-        if(player.getposicionX()-1<0 || player.getposicionY()-1<=0){}
-        else{
-        player.setposiciones(player.getposicionX()-1,player.getposicionY()-1);
-        }
-    }else if(action=="drd"){
-        if(player.getposicionX()+1>=board_X || player.getposicionY()+1>=board_Y-1){}
-        else{
-        player.setposiciones(player.getposicionX()+1,player.getposicionY()+1);
-        }
-    }else if(action=="dld"){
-        if(player.getposicionX()-1<0 || player.getposicionY()+1>=board_Y-1){}
-        else{
-        player.setposiciones(player.getposicionX()-1,player.getposicionY()+1);
-        }
-    }
-    else{
-        cout<<"Please type a valid movement, you lost a turn"<<endl;
-    }
-    //checar si golpeo la moneda
+void checkImpact(){
     for(int i=0;i<3;i++){
         if(player.getposicionX()==okane[i].getposicionX() && player.getposicionY()==okane[i].getposicionY()){
                 okane[i].setPisoteado();
@@ -196,11 +124,106 @@ void *PlayerMove(void *arg){
         }
     }
     checkGameOver();
-    pthread_exit(NULL);
+}
+void *PlayerMove(void *arg){
+    pthread_mutex_lock( &mutex);
+    string action;
+    checkImpact();
+    getline(cin,action);
+
+    if(action=="r"){
+        if(player.getposicionX()+1>=board_X){}
+        else{
+            player.setposiciones(player.getposicionX()+1,player.getposicionY());
+            moved = true;
+        }
+    }
+    else if(action=="2r"){
+            if(player.getposicionX()+2>=board_X){
+            }else{
+                player.setposiciones(player.getposicionX()+2,player.getposicionY());
+                moved = true;
+            }
+    }
+    else if(action=="l"){
+            if(player.getposicionX()-1<0){}
+            else{
+                player.setposiciones(player.getposicionX()-1,player.getposicionY());
+                moved = true;
+            }
+    }
+    else if(action=="2l"){
+            if(player.getposicionX()-2<0){}
+            else{
+                player.setposiciones(player.getposicionX()-2,player.getposicionY());
+                moved = true;
+            }
+    }
+    else if(action=="u"){
+        if(player.getposicionY()-1<=0){}
+        else{
+            player.setposiciones(player.getposicionX(),player.getposicionY()-1);
+            moved = true;
+        }
+    }
+    else if(action=="2u"){
+            if(player.getposicionY()-2<=0){}
+            else{
+            player.setposiciones(player.getposicionX(),player.getposicionY()-2);
+            moved = true;
+            }
+    }
+    else if(action=="d"){
+        if(player.getposicionY()+1>=board_Y-1){}
+        else{
+        player.setposiciones(player.getposicionX(),player.getposicionY()+1);
+        moved = true;
+        }
+    }
+    else if(action=="2d"){
+        if(player.getposicionY()+2>=board_Y-1){}
+        else{
+        player.setposiciones(player.getposicionX(),player.getposicionY()+2);
+        moved = true;
+        }    
+    }
+    else if(action=="dru"){
+        if(player.getposicionX()+1>=board_X || player.getposicionY()-1<=0){}
+        else{
+        player.setposiciones(player.getposicionX()+1,player.getposicionY()-1);
+        moved = true;
+        }
+    }else if(action=="dlu"){
+        if(player.getposicionX()-1<0 || player.getposicionY()-1<=0){}
+        else{
+        player.setposiciones(player.getposicionX()-1,player.getposicionY()-1);
+        moved = true;
+        }
+    }else if(action=="drd"){
+        if(player.getposicionX()+1>=board_X || player.getposicionY()+1>=board_Y-1){}
+        else{
+        player.setposiciones(player.getposicionX()+1,player.getposicionY()+1);
+        moved = true;
+        }
+    }else if(action=="dld"){
+        if(player.getposicionX()-1<0 || player.getposicionY()+1>=board_Y-1){}
+        else{
+        player.setposiciones(player.getposicionX()-1,player.getposicionY()+1);
+        moved = true;
+        }
+    }
+    else{
+        cout<<"Please type a valid movement, you lost a turn"<<endl;
+        moved = true;
+        system("pause");
+    }
+    checkImpact();
+    pthread_mutex_unlock( &mutex);
 }
 
 void *enemyMove(void *arg){
     srand(time(NULL));
+    checkImpact();
     for(int i=0;i<2;i++){
         int coin_flip = (1 + rand() % 4);
 
@@ -226,22 +249,18 @@ void *enemyMove(void *arg){
             }
         }
     }
-    for(int i=0;i<2;i++){
-        if(enemigo[i].getposicionX()==player.getposicionX() && enemigo[i].getposicionY()==player.getposicionY()){
-                enemigo[i].setCrash();
-                player.setCrash();
-        }
-    }
-    checkGameOver();
+    checkImpact();
     pthread_exit(NULL);
-    
 }
 void *ClockDown(void *arg){
-    for(;;){
-        //this_thread::sleep_for(chrono::seconds(30));
-    }
+    Sleep(10000);
+    movement_counter--;
+    system("cls");
+    FillGameBoard();
 
+    pthread_exit(NULL);
 }
+
 int getScore(){
     int puntaje=0;
     if(didWon==true){
@@ -259,27 +278,26 @@ int getScore(){
 
 int main(){
     srand(time(NULL));
-    vector<vector<string>> board;
+    pthread_t jikan;
     pthread_t jugador;
     pthread_t enemies[2];
-    pthread_t jikan;
     int action, action2, action3, tiempo;
     string newUser;
     
-    GenerateObjects(okane, SafeSpace, enemigo, player);
+    GenerateObjects();
 
     while(!GameOver){
+        moved =false;
         checkGameOver();
-        instructions();
-        FillGameBoard(board,okane,SafeSpace, enemigo, player);
-        cout<<"\n \n"<<movement_counter<<" Turns left"<<endl;
+        FillGameBoard();
         //tiempo = pthread_create(&jikan,NULL,&ClockDown,NULL);
-        action2 = pthread_create(&enemies[0],NULL,&enemyMove,NULL);
-        pthread_join(enemies[0], NULL);
         action = pthread_create(&jugador,NULL,&PlayerMove,NULL);
         pthread_join(jugador,NULL);
+        action2 = pthread_create(&enemies[0],NULL,&enemyMove,NULL);
+        pthread_join(enemies[0], NULL);
         movement_counter--;
         checkGameOver();
+
         system("cls");
     }
 
